db2b0181afe75d8fa97515c8f17f80845cf650fc
luotianchun <luotianchun123@qq.com>
2022-06-01 15:18:33 +0800

    test:action1

--- __test__/index.test.ts
+++ __test__/index.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from 'vitest'
-const add:Function = require('@luotianchun/hello-githubpackage')
+const add:Function = require('../build/bundle.cjs')
 test('test', () => {
   expect(add(1,3)).toBe(4)
 })
