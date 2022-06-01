6210f79198dbab68988ddae8b5ecf8188fc808c4
luotianchun <luotianchun123@qq.com>
2022-06-01 14:13:38 +0800

    feat: publish config

--- __test__/index.test.ts
+++ __test__/index.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from 'vitest'
-const add:Function = require('../build/bundle.cjs')
+const add:Function = require('@luotianchun/hello-githubpackage')
 test('test', () => {
   expect(add(1,3)).toBe(4)
 })
